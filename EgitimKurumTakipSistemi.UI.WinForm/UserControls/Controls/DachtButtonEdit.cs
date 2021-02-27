408f6fb26ed7765fa4974d5cb4832988c12cc04b
Erdem Altıparmak <erdem52ap@gmail.com>
2021-02-27 19:07:25 +0300

    ButtonEdit customize edildi

---
+++ EgitimKurumTakipSistemi.UI.WinForm/UserControls/Controls/DachtButtonEdit.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+using EgitimKurumTakipSistemi.UI.WinForm.UserControls.Interfaces;
+using System.Drawing;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.UserControls.Controls
+{
+    class DachtButtonEdit : ButtonEdit, IStatusBarAciklama
+    {
+        public DachtButtonEdit()
+        {
+            Properties.TextEditStyle = TextEditStyles.DisableTextEditor;
+            Properties.AppearanceFocused.BackColor = Color.LightBlue;
+        }
+        //public override bool EnterMoveNextControl { get; set; } = true;
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string StatusBarAciklama { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string StatusBarKisayol { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string StatusBarKisayolAciklama { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
---
+++ EgitimKurumTakipSistemi.UI.WinForm/UserControls/Interfaces/IStatusBarAciklama.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.UserControls.Interfaces
+{
+    public interface IStatusBarAciklama
+    {
+         string StatusBarAciklama { get; set; }
+         string StatusBarKisayol { get; set; }
+         string StatusBarKisayolAciklama { get; set; }
+
+    }
+}
