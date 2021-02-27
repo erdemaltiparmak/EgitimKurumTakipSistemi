9629d3a7e2b97a080311def4e7d94e5042ab9983
Erdem Altıparmak <erdem52ap@gmail.com>
2021-02-27 11:34:37 +0300

    Proje dosyası ekle.

---
+++ EgitimKurumTakipSistemi.UI.WinForm/Forms/BaseForms/BaseCardUC.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.Forms.BaseForms
+{
+    public partial class BaseCardUC : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public BaseCardUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
