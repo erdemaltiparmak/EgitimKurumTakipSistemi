3cb4e77598edcaf9912330a4ef64c1eea30cb550
Erdem Altıparmak <erdem52ap@gmail.com>
2021-02-27 11:38:55 +0300

    ..

---
+++ WpfBrowserApp1/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WpfBrowserApp1
+{ 
+    /// <summary>
+    /// App.xaml etkileşim mantığı
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
