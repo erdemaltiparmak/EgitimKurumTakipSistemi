52d6e6dd04483b544a6d007ede2d7a21da9c0033
Erdem Altıparmak <erdem52ap@gmail.com>
2021-03-24 19:48:40 +0300

    email text edit

---
+++ EgitimKurumTakipSistemi.UI.WinForm/UserControls/Controls/DachtEMailTextEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.UserControls.Controls
+{
+    [ToolboxItem(true)]
+     public class DachtEMailTextEdit:DachtTextEdit
+    {
+        public DachtEMailTextEdit()
+        {
+            Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.RegEx;
+            Properties.Mask.EditMask = @"((([0-9a-zA-Z_%-])+[.])+|([0-9a-zA-Z_%-])+)+@((([0-9a-zA-Z_-])+[.])+|([0-9a-zA-Z_-])+)+";
+            Properties.Mask.AutoComplete = DevExpress.XtraEditors.Mask.AutoCompleteType.Strong;
+        }
+    }
+}
