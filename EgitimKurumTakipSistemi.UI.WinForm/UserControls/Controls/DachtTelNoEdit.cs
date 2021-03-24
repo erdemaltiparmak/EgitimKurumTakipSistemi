b9dda36c7e8db20a8901c2111c0cce0a949db3e7
Erdem Altıparmak <erdem52ap@gmail.com>
2021-03-24 13:25:16 +0300

    Contact number mask added

---
+++ EgitimKurumTakipSistemi.UI.WinForm/UserControls/Controls/DachtTelNoEdit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class DachtTelNoEdit:DachtTextEdit
+    {
+        public DachtTelNoEdit()
+        {
+            Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Regular;
+            Properties.Mask.EditMask = @"0(\d?\d?\d?)-\d?\d?\d?-\d?\d?\d?\d?";
+            Properties.Mask.AutoComplete = DevExpress.XtraEditors.Mask.AutoCompleteType.None;
+            StatusBarAciklama = "Telefon No Giriniz";
+        }
+    }
+}
