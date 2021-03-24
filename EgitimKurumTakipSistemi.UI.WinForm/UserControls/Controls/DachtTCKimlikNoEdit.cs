48dfa3f3da330cb1dfaeae31543e287d1e16fa74
Erdem Altıparmak <erdem52ap@gmail.com>
2021-03-24 12:08:29 +0300

    TCKNO

---
+++ EgitimKurumTakipSistemi.UI.WinForm/UserControls/Controls/DachtTCKimlikNoEdit.cs
@@ -0,0 +1,23 @@
+﻿using EgitimKurumTakipSistemi.UI.WinForm.UserControls.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgitimKurumTakipSistemi.UI.WinForm.UserControls.Controls
+{
+    [ToolboxItem(true)]
+    public class DachtTCKimlikNoEdit : DachtTextEdit
+    {
+        public DachtTCKimlikNoEdit()
+        {
+            Properties.Mask.MaskType = DevExpress.XtraEditors.Mask.MaskType.Regular;
+            Properties.Mask.EditMask = @"\d?\d?\d?\d?\d?\d?\d?\d?\d?\d?\d?";
+            Properties.Mask.AutoComplete = DevExpress.XtraEditors.Mask.AutoCompleteType.None;
+            StatusBarAciklama = "TC Kimlik No Giriniz";
+        }
+        public override bool EnterMoveNextControl { get; set; } = true;
+    }
+}
